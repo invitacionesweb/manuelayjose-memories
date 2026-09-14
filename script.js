10e0d3aa94072db66dd7c560f576d875458efd92
invitacionesweb <invitacionesweb.info@gmail.com>
2026-09-14 16:26:13 -0300

    abc
    abc

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Establecer la fecha de destino
-const fechaDestino = new Date("2026-12-19T23:59:59");
+const fechaDestino = new Date("2026-12-19T00:01:59");
 
 // Variable para almacenar el ID del intervalo
 let intervalo;
